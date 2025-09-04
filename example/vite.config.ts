2ed5566741552c3c8d683ed411cc856337d2ba79
Rafa <rafael@avena.io>
2025-09-03 22:34:49 -0600

    feat: add example vite configuration and also add run locally instructions on dev

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    // evita React duplicado entre DS y example
+    dedupe: ["react", "react-dom"],
+    // si notas comportamientos raros con symlinks, puedes probar:
+    // preserveSymlinks: true,
+  },
+  server: {
+    // permite acceder a archivos fuera de /example (tu DS)
+    fs: { allow: [".."] },
+  },
+  optimizeDeps: {
+    // evita que Vite pre-empaque el DS; así lo trata como "source" y hace HMR
+    exclude: ["@ralorotech/rl-design-system"],
+  },
+});
